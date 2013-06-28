e6b6969e0f18b3fee996623b7d560267ee885f0d
Mirko Velic <mvelic@smsetechnologies.com>
2013-06-28 12:14:22 +1000

    Added working Django setup with base template and test views.

--- md_manager/manager/urls.py
+++ md_manager/manager/urls.py
@@ -3,5 +3,8 @@ from django.conf.urls import patterns, include, url
 from manager import views
 
 urlpatterns = patterns('',
-   	url(r'^$', views.index, name='index'),
+   	url(r'^$', views.IndexView.as_view(), name='index'),
+   	url(r'^active_jobs$', views.active_jobs, name='active_jobs'),
+   	url(r'^active_simulations$', views.active_simulations, name='active_simulations'),
+   	url(r'^create_simulation$', views.create_simulation, name='create_simulation'),
 )
--- md_manager/manager/views.py
+++ md_manager/manager/views.py
@@ -1,5 +1,16 @@
 # manager views
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.shortcuts import render, render_to_response, get_object_or_404, redirect
+from django.views import generic
 
-def index(request):
-	return HttpResponse("blah")
+class IndexView(generic.TemplateView):
+	template_name = "index.html"
+
+def active_jobs(request):
+	return render_to_response("job_views/active_jobs.html", { "blah": "test var" })
+
+def active_simulations(request):
+	return HttpResponse("Dummy Response")
+
+def create_simulation(request):
+	return HttpResponse("Dummy Response")
