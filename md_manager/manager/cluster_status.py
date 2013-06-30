9512d506cb895efb2eb11cb3fb08cbf71325321e
Ben Porebski <ben@inoxx.net>
2013-06-30 10:38:49 +1000

    Added cluster status page

---
+++ md_manager/manager/cluster_status.py
@@ -0,0 +1,63 @@
+from manager.models import ClusterHost
+import subprocess
+import xml.etree.ElementTree as ET
+
+def get_status():
+
+	host_list = []
+
+	for host in ClusterHost.objects.all():
+
+		## Will one day need to handle multiple communication pipelines.
+		## In the event it is locally hosted on a cluster, should just call qstat directly
+
+		command = "ssh %s@%s %s -x" % (host.username, host.hostname, host.qstat_cmd)
+		qstat_xml = subprocess.check_output(command, shell=True)
+
+		root = ET.fromstring(qstat_xml)
+
+		job_list = []
+
+		for child in root:
+			job_dict = {}
+
+			job_id = child.find("Job_Id").text.split(".")[0]
+			job_name = child.find("Job_Name").text
+			job_owner = child.find("Job_Owner").text.split("@")[0]
+			try:
+				nodes = child.find("Resource_List").find("nodes").text
+				n_nodes = int(nodes.split(':')[0])
+			except:
+				n_nodes = 1
+			try:
+				ppn = int(nodes.split(':')[1].split('=')[1])
+				cores = n_nodes*ppn
+			except:
+				cores = n_nodes
+			try:
+				work_dir = child.find("init_work_dir").text
+			except:
+				work_dir = ""
+
+			job_dict['job_id'] = job_id
+			job_dict['job_name'] = job_name
+			job_dict['job_owner'] = job_owner
+			job_dict['nodes'] = nodes
+			job_dict['n_nodes'] = n_nodes
+			job_dict['ppn'] = ppn
+			job_dict['cores'] = cores
+			job_dict['work_dir'] = work_dir
+
+
+			##job_list.append((job_id, job_name, job_owner, cores, work_dir))
+
+			# if host_index == 0:
+			# 	job_dict['collapse'] = "collapse in"
+			# else:
+			# 	job_dict['collapse'] = "collapse"
+
+			job_list.append(job_dict)
+
+		host_list.append((host, job_list))
+
+	return host_list
