1cfcbf3aa87c047cd341682e5dde63b9e626eb28
Ben Porebski <ben@inoxx.net>
2013-08-14 12:03:16 +1000

    Cleaned up and modularised hpc functionality and simulation software functionality. Removed hpc.py, cluster_status.py and simulations.py. Migrated code into the model, view and created separate files for specific software.

--- md_manager/manager/views.py
+++ md_manager/manager/views.py
@@ -8,33 +8,56 @@ class IndexView(generic.TemplateView):
 
 def cluster_status(request):
 	import cluster_status
-	##cluster_status_context = get_cluster_status()
-	##print cs.get_status()[0][0]
-	return render_to_response("job_views/cluster_status.html", { "cluster_status": cluster_status.get_status() })
+	from manager.models import ClusterHost
+	from manager.models import ClusterJob
+
+	## Update ClusterJobs for all ClusterHosts
+	## Eventually move this to a button...
+	for host in ClusterHost.objects.all():
+		host.update_queue()
+
+	return render_to_response("job_views/cluster_status.html", { "clusterjobs": ClusterJob.objects.all(),
+																	"clusterhosts": ClusterHost.objects.all() })
+
+
 
 def view_simulations(request):
 	from manager.models import Simulation
 	from manager.models import Project
 	return render_to_response("job_views/view_simulations.html", { "projects": Project.objects.all(),
 																	"simulations": Simulation.objects.all() })
-def update_simulations(request, sim_id = None):
+def update_simulations(request, project_id = None):
 	from django.http import HttpResponseRedirect
-	import simulations
+	from manager.models import Project
+	from manager.models import Simulation
+	from manager.models import ClusterHost
+
+	## Update all clusters first
+	for host in ClusterHost.objects.all():
+		host.update_queue()
+
 	## Perform the update here
-	if sim_id == None:
+	if project_id == None:
 		## Update all simulations
-		simulations.update_status_all()
+		for project in Project.objects.all():
+			for sim in project.simulation_set.all():
+				sim.update_simulation()
 	else:
-		## Update project
-		simulations.update_status(int(sim_id))
+		for sim in Project.objects.get(id=project_id).simulation_set.all():
+		## Update specific project
+			sim.update_simulation()
+
 	## Redirect to view_simulations page
 	return HttpResponseRedirect("/view_simulations")
 
 
-def view_analysis(request, sim_id):
+
+
+
+def view_analysis(request, project_id):
 	from manager.models import Project
 
-	return render_to_response("analysis_views/view_analysis.html", { "project": Project.objects.get(id=sim_id) })
+	return render_to_response("analysis_views/view_analysis.html", { "project": Project.objects.get(id=project_id) })
 
 
 
